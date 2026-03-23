93b7265367d53d53265042448889eb29584d1dfa
Akash <akashman00001@gmail.com>
2026-03-23 17:32:59 +0530

    Initial commit

---
+++ src/main/java/com/example/Food/Application/Backend/FoodApplicationBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.Food.Application.Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodApplicationBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodApplicationBackendApplication.class, args);
+	}
+
+}
